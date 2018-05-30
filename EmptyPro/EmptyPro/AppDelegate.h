53df3e0de13bbbe99946365b2c7d6464d6b014c6
bq <584203135@qq.com>
2018-05-30 17:04:38 +0800

    添加程序代码

---
+++ EmptyPro/EmptyPro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EmptyPro
+//
+//  Created by 毕强 on 2018/5/30.
+//  Copyright © 2018年 rushsnail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
